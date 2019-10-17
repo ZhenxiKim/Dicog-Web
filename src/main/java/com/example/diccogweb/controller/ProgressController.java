8c7368bb585a4e83a0368064caadcb595469ee1a
8055kjh <8055kjh@gmail.com>
2019-10-17 18:57:05 +0900

    받아쓰기진행상황 api 구현중

---
+++ src/main/java/com/example/diccogweb/controller/ProgressController.java
@@ -0,0 +1,26 @@
+package com.example.diccogweb.controller;
+
+import com.example.diccogweb.service.ProgressService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/progresses",, consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_JSON_VALUE})
+public class ProgressController {
+    //맞춘거체크,진행상황체크
+    private final ProgressService progressService;
+    public ProgressController(ProgressService progressService){
+        this.progressService = progressService;
+    }
+
+    @ApiOperation(value="받아쓰기 진행상황")
+    @GetMapping("/{memSn}/{step}")
+    public ResponseEntity<?> checkProgress(@PathVariable Long memSn,String step){
+        
+    }
+}
---
+++ src/main/java/com/example/diccogweb/service/ProgressService.java
@@ -0,0 +1,4 @@
+package com.example.diccogweb.service;
+
+public class ProgressService {
+}
